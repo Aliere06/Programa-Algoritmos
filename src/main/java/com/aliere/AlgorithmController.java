34ba9971c2c4a84bbf9906b4a7751adedaaa03fe
Aliere <allieig666@gmail.com>
2024-10-04 09:18:50 -0600

    table preparations

--- src/main/java/com/aliere/AlgorithmController.java
+++ src/main/java/com/aliere/AlgorithmController.java
@@ -1,13 +1,19 @@
 package com.aliere;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 
 public class AlgorithmController{
 
     @FXML private Label titleLabel;
     @FXML private VBox parameterVBox;
+    @FXML private TableView<String> tableView;
 
     public void setTitle(String text) {
         titleLabel.setText(text);
--- src/main/java/com/aliere/MenuController.java
+++ src/main/java/com/aliere/MenuController.java
@@ -2,11 +2,15 @@ package com.aliere;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.MapValueFactory;
 
 public class MenuController implements Initializable{
 
